0130a7da2db8be6cbdab0f20bb89dd63a94f1b93
LGD_Sunday <12055071971@qq.com>
2022-03-15 19:02:30 +0800

    安装 tailwindcss 到你的项目

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img class="bg-red-900" alt="Vue logo " src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
