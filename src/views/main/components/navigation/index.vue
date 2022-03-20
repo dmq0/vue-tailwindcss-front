a1bdea1a872767af776fb9c8e6657b9fe540a811
LGD_Sunday <12055071971@qq.com>
2022-03-20 17:56:08 +0800

    导入并配置 axios

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -5,6 +5,13 @@
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
 import mobileNavigationVue from './mobile/index.vue'
+import { getCategory } from '@/api/category'
+
+const getCategoryData = async () => {
+  const res = await getCategory()
+  console.log(res)
+}
+getCategoryData()
 </script>
 
 <style lang="scss" scoped></style>
