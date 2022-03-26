7ab11daaa2d6678f6340c2fdda762cc55414386a
LGD_Sunday <12055071971@qq.com>
2022-03-26 21:33:47 +0800

    动态 rem 基准+修正 tailwind，完善移动端navigationBar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { useREM } from './utils/flexible'
 import './styles/index.scss'
-
+// 设置 rem
+useREM()
 createApp(App).use(router).mount('#app')
