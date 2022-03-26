70952087b90f660fb496c5c14f5f4f7625a4c39e
LGD_Sunday <12055071971@qq.com>
2022-03-26 18:42:11 +0800

    数据拦截简化数据获取流程

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,4 +5,16 @@ const service = axios.create({
   timeout: 5000
 })
 
+// 响应拦截器
+service.interceptors.response.use((response) => {
+  const { success, message, data } = response.data
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // TODO：业务错误
+    return Promise.reject(new Error(message))
+  }
+})
+
 export default service
