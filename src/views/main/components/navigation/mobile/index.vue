309da462762de3c66a444f3ed562ed13bdcbb6eb
LGD_Sunday <12055071971@qq.com>
2022-03-27 17:59:15 +0800

    业务组件：移动端弹层 menu

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -34,7 +34,7 @@
     </ul>
 
     <m-popup v-model="isOpenPopup">
-      <div>测试内容</div>
+      <menu-vue :categorys="data" @onItemClick="onItemClick"></menu-vue>
     </m-popup>
   </div>
 </template>
@@ -42,6 +42,7 @@
 <script setup>
 import { ref, watch, onBeforeUpdate } from 'vue'
 import { useScroll } from '@vueuse/core'
+import MenuVue from '@/views/main/components/menu/index.vue'
 
 defineProps({
   data: {
@@ -83,6 +84,7 @@ watch(currentCategoryIndex, (val) => {
 // item 点击事件
 const onItemClick = (index) => {
   currentCategoryIndex.value = index
+  isOpenPopup.value = false
 }
 
 // popup 展示
