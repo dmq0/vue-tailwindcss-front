244ed6d5e6260a159da2c55e6a50106c9f73fbd3
LGD_Sunday <12055071971@qq.com>
2022-03-27 14:45:44 +0800

    通用组件：基于 vite 的统一 svg 处理  svg-icon

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import mLibs from './libs'
 import { useREM } from './utils/flexible'
 import './styles/index.scss'
+
 // 设置 rem
 useREM()
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(mLibs).mount('#app')
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -3,6 +3,12 @@
     <ul
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
     >
+      <!-- 汉堡按钮 -->
+      <li
+        class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
+      >
+        <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
+      </li>
       <!-- category item -->
       <li
         v-for="item in data"
