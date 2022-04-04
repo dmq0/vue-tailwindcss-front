d8fb4ab068e151348f076a0dc289314a7cdbe037
LGD_Sunday <12055071971@qq.com>
2022-04-04 21:22:19 +0800

    searchBar ：处理防抖功能

--- src/views/layout/components/header/header-search/hint.vue
+++ src/views/layout/components/header/header-search/hint.vue
@@ -18,6 +18,7 @@ const EMITS_ITEM_CLICK = 'itemClick'
 <script setup>
 import { getHint } from '@/api/pexels'
 import { ref, watch } from 'vue'
+import { watchDebounced } from '@vueuse/core'
 
 /**
  * 接收搜索数据
@@ -45,8 +46,10 @@ const getHintData = async () => {
 /**
  * 监听搜索文本的变化，并获取对应提示数据
  */
-watch(() => props.searchText, getHintData, {
-  immediate: true
+watchDebounced(() => props.searchText, getHintData, {
+  immediate: true,
+  // 每次事件触发时，延迟的时间
+  debounce: 500
 })
 
 /**
