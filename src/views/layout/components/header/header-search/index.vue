7db6ec17a481cc6e060a6a15f2b07161873a2708
LGD_Sunday <12055071971@qq.com>
2022-04-04 20:13:59 +0800

    searchBar 功能提示：搜索提示初步实现

--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="w-full">
-    <m-search v-model="inputValue">
+    <m-search
+      v-model="inputValue"
+      @search="onSearchHandler"
+      @clear="onSearchHandler"
+    >
       <template #dropdown>
-        <div>dropdown</div>
+        <div>
+          <!-- 搜索提示 -->
+          <hint-vue
+            v-show="inputValue"
+            :searchText="inputValue"
+            @itemClick="onHintItemClick"
+          ></hint-vue>
+        </div>
       </template>
     </m-search>
   </div>
@@ -10,8 +21,21 @@
 
 <script setup>
 import { ref } from 'vue'
+import hintVue from './hint.vue'
 
 const inputValue = ref('')
+
+// 搜索的回调事件
+const onSearchHandler = (val) => {
+  inputValue.value = val
+}
+
+/**
+ * 搜索结果点击事件处理
+ */
+const onHintItemClick = (item) => {
+  onSearchHandler(item)
+}
 </script>
 
 <style lang="scss" scoped></style>
