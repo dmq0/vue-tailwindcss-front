20ebe85c57dbba17eabe69eb8ac54a5054d512da
LGD_Sunday <12055071971@qq.com>
2022-04-09 21:31:05 +0800

    注册页面基本样式处理

--- src/views/login-register/login/index.vue
+++ src/views/login-register/login/index.vue
@@ -47,7 +47,7 @@
         <div class="pt-1 pb-3 leading-[0px] text-right">
           <a
             class="inline-block p-1 text-zinc-400 text-right dark:text-zinc-600 hover:text-zinc-600 dark:hover:text-zinc-400 text-sm duration-400 cursor-pointer"
-            target="__black"
+            @click="onToRegister"
           >
             去注册
           </a>
@@ -142,6 +142,13 @@ const onLogin = async () => {
     loading.value = false
   }
   router.push('/')
+}
+
+/**
+ * 进入注册页面
+ */
+const onToRegister = () => {
+  router.push('/register')
 }
 </script>
 
