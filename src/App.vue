3cb7920e8f95ace169e9f3f562be607bc8af1115
LGD_Sunday <12055071971@qq.com>
2022-04-13 16:15:29 +0800

    通用组件：虚拟任务栈处理

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <div class="h-screen fixed top-0 left-0 w-screen">
     <!-- <router-view></router-view> -->
     <m-transition-router-view
+      mainComponentName="home"
       :routerType="$store.getters.routerType"
     ></m-transition-router-view>
   </div>
--- src/libs/transition-router-view/index.vue
+++ src/libs/transition-router-view/index.vue
@@ -8,10 +8,11 @@
       @after-leave="afterLeave"
     >
       <!-- 缓存组件 -->
-      <keep-alive>
+      <keep-alive :include="virtualTaskStack">
         <component
           :is="Component"
-          :class="{ 'fixed top-0 left-0 w-screen': isAnimation }"
+          :class="{ 'fixed top-0 left-0 w-screen z-50': isAnimation }"
+          :key="$route.fullPath"
         />
       </keep-alive>
     </transition>
@@ -50,9 +51,13 @@ const props = defineProps({
   },
   // 首页的组件名称，对应任务栈中的第一个组件
   mainComponentName: {
-    type: String
+    type: String,
+    required: true
   }
 })
+
+// 任务栈
+const virtualTaskStack = ref([props.mainComponentName])
 
 const router = useRouter()
 // 跳转动画
@@ -63,6 +68,19 @@ const transitionName = ref('')
 router.beforeEach((to, from) => {
   // 定义当前动画名称
   transitionName.value = props.routerType
+
+  if (props.routerType === PUSH) {
+    // 入栈
+    virtualTaskStack.value.push(to.name)
+  } else if (props.routerType === BACK) {
+    // 出栈
+    virtualTaskStack.value.pop()
+  }
+
+  // 进入首页默认清空栈
+  if (to.name === props.mainComponentName) {
+    clearTask()
+  }
 })
 
 // 处理动画状态变化
@@ -73,18 +91,25 @@ const beforeEnter = () => {
 const afterLeave = () => {
   isAnimation.value = false
 }
+
+/**
+ * 清空栈
+ */
+const clearTask = () => {
+  virtualTaskStack.value = [props.mainComponentName]
+}
 </script>
 
 <style lang="scss" scoped>
 // push页面时：新页面的进入动画
 .push-enter-active {
   animation-name: push-in;
-  animation-duration: 0.5s;
+  animation-duration: 0.4s;
 }
 // push页面时：老页面的退出动画
 .push-leave-active {
   animation-name: push-out;
-  animation-duration: 0.5s;
+  animation-duration: 0.4s;
 }
 // push页面时：新页面的进入动画
 @keyframes push-in {
@@ -108,12 +133,12 @@ const afterLeave = () => {
 // 后退页面时：即将展示的页面动画
 .back-enter-active {
   animation-name: back-in;
-  animation-duration: 0.5s;
+  animation-duration: 0.4s;
 }
 // 后退页面时：后退的页面执行的动画
 .back-leave-active {
   animation-name: back-out;
-  animation-duration: 0.5s;
+  animation-duration: 0.4s;
 }
 // 后退页面时：即将展示的页面动画
 @keyframes back-in {
