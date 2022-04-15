728b3d0dd9812470e6ce36d023bb10bfaa62950e
LGD_Sunday <12055071971@qq.com>
2022-04-15 19:49:45 +0800

    移动端 QQ 登录对接：触发吊起操作，完成移动端 QQ 登录

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -5,9 +5,14 @@ import { useWindowSize } from '@vueuse/core'
 const { width } = useWindowSize()
 
 /**
- * 判断当前是否为移动设备，判断依据为屏幕的宽度是否大于指定值（1280）
+ * 判断当前是否为移动设备
  */
 export const isMobileTerminal = computed(() => {
+  // 方式1：
+  // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  //   navigator.userAgent
+  // )
+  // 方式2：
   return width.value < PC_DEVICE_WIDTH
 })
 
