c204ac7a7b84f86cb4a28d814bf38b592ec4c1f3
LGD_Sunday <12055071971@qq.com>
2022-04-15 17:51:01 +0800

    QQ 登录：获取QQ用户信息

--- src/views/login-register/login/qq-login.vue
+++ src/views/login-register/login/qq-login.vue
@@ -28,7 +28,20 @@ onMounted(() => {
     // 登录存在缓存，登录成功一次之后，下次进入会自动重新登录（即：触发该方法，所以我们应该在离开登录页面时，注销登录）
     (data, opts) => {
       console.log('QQ登录成功')
-      console.log(data)
+      // 1. 注销登录，否则在后续登录中会直接触发该回调
+      QC.Login.signOut()
+      // 2. 获取当前用户唯一标识，作为判断用户是否已注册的依据
+      const accessToken = /access_token=((.*))&expires_in/.exec(
+        window.location.hash
+      )[1]
+      // 3. 拼接请求对象
+      const oauthObj = {
+        nickname: data.nickname,
+        figureurl_qq_2: data.figureurl_qq_2,
+        accessToken
+      }
+      console.log(oauthObj)
+      // TODO：4. 完成跨页面传输
     }
   )
 })
